f7fc602c5a8ea5709225b747f2ff19824ed5ab06
JessyHanifiah <72622302+JessyHanifiah@users.noreply.github.com>
2021-02-28 21:12:07 +0700

    Delete myScript.js

--- pertemuan-2/myScript.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
-
-  function myFunction2() {
-    document.getElementById("demo").innerHTML = "paragraf setelah diubah JavaScript";
-  }
