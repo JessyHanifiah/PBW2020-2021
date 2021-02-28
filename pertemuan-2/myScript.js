0bb4236663c3fb64ef5c716dad1a8ac5b92300e7
Jessy Hanifiah <jessyhanifiah@gmail.com>
2021-02-28 11:23:20 +0700

    rename directory

---
+++ pertemuan-2/myScript.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+  }
+
+  function myFunction2() {
+    document.getElementById("demo").innerHTML = "paragraf setelah diubah JavaScript";
+  }
